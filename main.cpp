37c6518ac26f3ce0ccc95ab86d82d620ff2c9e37
Adam Dyrda <a.dyrda102@gmail.com>
2025-01-26 19:59:37 +0100

    updated main

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
-#include <iostream>
+
+/**
+ * @file main.cpp
+ * @brief Main file of the project
+ * @details This file contains the main function of the project. It reads user input and runs the program function.
+ */
+
+#include <fstream>
+#include <vector>
+#include "inc/geneticAlgorythm.h"
+#include "inc/item.h"
+#include "inc/utilities.h"
+#include "inc/chromosome.h"
+#include "inc/program.h"
+#include "inc/userInput.h"
 
 
 
 
-int main(int argc, char *argv[])
-{
-    std::string inputFileName;
-    std::string outputFileName;
-    float capacity;
-    int numberOfGenerations;
-    int numberOfIndividuals;
-for (int i = 1; i < argc; i++) {
-    std::string arg = argv[i];
-    if(arg=="-i") {inputFileName = argv[++i];}
-    else if(arg=="-o") {outputFileName = argv[++i];}
-    else if(arg=="-c"){capacity = atof(argv[++i]);}
-    else if(arg=="-g"){numberOfGenerations = atoi(argv[++i]);}
-    else if(arg=="-n"){numberOfIndividuals = atoi(argv[++i]);}
-}
+int main(int argc, char *argv[]) {
+
+    UserInput userInput = utilities::parseUserInput(argc, argv);
+    program::runProgram(userInput);
     return 0;
+
+
 }
